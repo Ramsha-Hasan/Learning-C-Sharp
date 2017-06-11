99d869db4b5110597da3626ba63141396c9f86ba
Ramsha Hasan <ramsha.hasan2@gmail.com>
2017-06-11 22:41:28 +0500

    Add files via upload

---
+++ nested for loop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace c(sharp)
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+Console.WriteLine("Multiplication Table");
+
+for  (int i = 1; i < 11; i++)
+{
+    for (int j = 1; j < 11; j++ )
+    {
+        var product = (i * j).ToString();
+        Console.Write(product.PadLeft(7));
+    
+    }
+    Console.WriteLine();
+}
+
+   
+        }
+    }
+}
